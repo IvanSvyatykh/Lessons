574fc1e08c4da7091442e0cc84b2ae099445061e
Ivan Svyatykh <Zlativansvyat@mail.ru>
2023-02-12 12:29:07 +0500

    Добавьте файлы проекта.

---
+++ Lessons/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.Dynamic;
+
+namespace Lessons
+{
+    public interface IHashable
+    {
+        int GetHash();
+    }
+    class MyInt : IHashable
+    {
+        public int Value;
+        public MyInt(int value) 
+        {
+            Value = value;
+        }
+        public int GetHash()
+        {
+            return Value.GetHashCode();
+        }
+    }
+    public class Set<TValue> : IEnumerable<TValue>
+        where TValue : IHashable
+    {
+        private List<TValue> _data { get; } = new List<TValue>();
+
+        public void Add(TValue value)
+        {
+            if (IsContains(value))
+            {
+                _data.Add(value);
+            }
+
+        }
+        private bool IsContains(TValue value)
+        {
+            foreach (var item in _data)
+            {
+                if (item.GetHash() == value.GetHash()) return false;
+            }
+            return true;
+        }
+        public IEnumerator<TValue> GetEnumerator()
+        {
+            return ((IEnumerable<TValue>)_data).GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return _data.GetEnumerator();
+        }
+
+        public TValue this[int index]
+        {
+            get
+            {
+                return _data[index];
+            }
+        }
+
+    }
+
+    public class Program
+    {
+        static void Main()
+        {
+            Set<MyInt> ints = new Set<MyInt>();
+            ints.Add(new MyInt(5));
+            ints.Add(new MyInt(6));
+            ints.Add(new MyInt(7));
+            ints.Add(new MyInt(5));
+            ints.Add(new MyInt(7));
+
+            foreach (var item in ints) 
+            {
+                Console.WriteLine(item.Value);    
+            }
+
+        }
+    }
+}
---
+++ Lessons/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Lessons")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Lessons")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("9a9e4237-b0d7-454a-8e79-7b8e4d1b05f0")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
